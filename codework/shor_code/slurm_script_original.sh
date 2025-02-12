3bf4134c53cce8723739564f7867b73680095855
Ak-ash22 <14akash2000@gmail.com>
2025-02-12 11:05:59 +0100

    cluster_trial

--- codework/shor_code/slurm_script_original.sh
+++ codework/shor_code/slurm_script_original.sh
@@ -5,7 +5,7 @@
 #SBATCH -e logs_auto_runner/%x_log.err
 #SBATCH --cpus-per-task=1
 #SBATCH --time=200:00:00
-#SBATCH --mem=4G
+#SBATCH --mem-per-cpu=4G
 #SBATCH -p epyc-256
 #SBATCH --mail-type=FAIL,END
 #SBATCH --array=0-1
