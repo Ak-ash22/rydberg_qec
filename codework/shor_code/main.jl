e72bff7f17d261497f8c760e644a892317acb27c
Ak-ash22 <14akash2000@gmail.com>
2025-02-14 14:18:46 +0100

    trying the cluster

--- codework/shor_code/main.jl
+++ codework/shor_code/main.jl
@@ -1,49 +1,49 @@
-include("functions.jl")
+# include("functions.jl")
 
 #Saving the output
-script_dir = "/scratch/roq68sum/shor_code_data"
+# script_dir = "/scratch/roq68sum/shor_code_data"
 
-data_folder = joinpath(script_dir, "driving_abc")
+# data_folder = joinpath(script_dir, "driving_abc")
 
-if !isdir(data_folder)
-    println("Directory does not exist. Creating directory...: $data_folder")
-    mkpath(data_folder)
-end
+# if !isdir(data_folder)
+#     println("Directory does not exist. Creating directory...: $data_folder")
+#     mkpath(data_folder)
+# end
 
 function main(N_trajectories::Int)
     """
     Main function to run the simulation
     """
-    println("Running the simulation for $N_trajectories")
+    # println("Running the simulation for $N_trajectories")
 
-    start_time = time()
+    # start_time = time()
 
-    basis = NLevelBasis(2)
-    k = transition(basis,2,2)
-    n_a = full_operator(k,total_qubits, [1])
-    n_c = full_operator(k, total_qubits, [3])
-    n_ac = full_operator(k, total_qubits, [1,3])
+    # basis = NLevelBasis(2)
+    # k = transition(basis,2,2)
+    # n_a = full_operator(k,total_qubits, [1])
+    # n_c = full_operator(k, total_qubits, [3])
+    # n_ac = full_operator(k, total_qubits, [1,3])
 
-    ψ0 = initialize_system()
-    println("The system has been initialized.")
+    # ψ0 = initialize_system()
+    # println("The system has been initialized.")
 
-    full_basis = CompositeBasis([NLevelBasis(2) for _ in 1:total_qubits]...)
-    ψ0_ket = Ket(full_basis, ComplexF32.(ψ0)) 
+    # full_basis = CompositeBasis([NLevelBasis(2) for _ in 1:total_qubits]...)
+    # ψ0_ket = Ket(full_basis, ComplexF32.(ψ0)) 
 
-    println("Starting the simulation...")
+    # println("Starting the simulation...")
 
-    @time tout, ψt = timeevolution.mcwf_dynamic(tspan,ψ0_ket,f;maxiters=1e9,seed=N_trajectories)
+    # @time tout, ψt = timeevolution.mcwf_dynamic(tspan,ψ0_ket,f;maxiters=1e9,seed=N_trajectories)
   
-    println("Trajectory $N_trajectories.\n")
+    # println("Trajectory $N_trajectories.\n")
     
-    population_a = real(expect(n_a, ψt))
-    population_c = real(expect(n_c, ψt))
-    population_ac = real(expect(n_ac, ψt))
+    # population_a = real(expect(n_a, ψt))
+    # population_c = real(expect(n_c, ψt))
+    # population_ac = real(expect(n_ac, ψt))
 
-    end_time = time() - start_time
-
+    # end_time = time() - start_time
+    print(1+1)
     println("Simulation complete. Saving data...")
-    @save "$(data_folder)/N_atoms=$(total_qubits)_γ_decay=$(γ_Decay)_Ntraj=$(N_trajectories).jld2" population_a population_c population_ac end_time
+    # @save "$(data_folder)/N_atoms=$(total_qubits)_γ_decay=$(γ_Decay)_Ntraj=$(N_trajectories).jld2" population_a population_c population_ac end_time
     println("Data saved.")
 end
 
@@ -51,6 +51,9 @@ end
 # --- Parse command-line arguments ---
 if abspath(PROGRAM_FILE) == @__FILE__
     if length(ARGS) < 1
+        println("Usage: julia main.jl <N_trajectories>")
+        exit(1)
+    elseif length(ARGS) > 1
         println("Usage: julia main.jl <N_trajectories>")
         exit(1)
     end
