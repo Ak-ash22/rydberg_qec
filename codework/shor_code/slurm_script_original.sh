35eb0c20954a41a64734072833617325ba0b2c19
Ak-ash22 <14akash2000@gmail.com>
2025-02-26 00:43:53 +0100

    step1-3 trial 3

--- codework/shor_code/slurm_script_original.sh
+++ codework/shor_code/slurm_script_original.sh
@@ -3,10 +3,10 @@
 #SBATCH -J q001b
 #SBATCH -o logs_auto_runner/%x_%a_log.out
 #SBATCH -e logs_auto_runner/%x_%a_log.err
-#SBATCH --cpus-per-task=20
+#SBATCH --cpus-per-task=16
 #SBATCH --time=20:00:00
 #SBATCH --mem-per-cpu=4G
-#SBATCH -p idle
+#SBATCH -p epyc-256
 #SBATCH --mail-type=FAIL,END
 #SBATCH --array=1-100
 
