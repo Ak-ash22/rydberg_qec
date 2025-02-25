b1f1a5a4eb24573c294b644d70e11a4b7b69d538
Ak-ash22 <14akash2000@gmail.com>
2025-02-25 11:39:45 +0100

    hadamard gate trial 3

--- codework/shor_code/system_params.jl
+++ codework/shor_code/system_params.jl
@@ -71,7 +71,7 @@ function params()
         :Δ1_0 => 1032.0,        # Detuning at t=0 for qubits
         # :Δ2_0 => 1032.0,        # Detuning at t=0 for ancillas
         :T1 => 154.0,    # Evolution time for step 1 -- From 2atom_optimal_decay_result.ipynb
-        :T2 => round(154.0+pi/2)     # Evolution time for step 2
+        :T2 => 154.0+pi/2     # Evolution time for step 2
         # :T3 => T2+154.0     # Evolution time for step 3
         # :T4 => 582.0     # Evolution time for step 4
         # :T5 => 582.0     # Evolution time for step 5  -- for phase flip error syndrome
