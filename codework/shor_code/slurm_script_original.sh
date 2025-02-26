ebbcbac3d85352f97cf0ce67298b5e04fa01875f
Ak-ash22 <14akash2000@gmail.com>
2025-02-26 21:12:20 +0100

    shor code cluster run 2

--- codework/shor_code/slurm_script_original.sh
+++ codework/shor_code/slurm_script_original.sh
@@ -3,12 +3,12 @@
 #SBATCH -J q001b
 #SBATCH -o logs_auto_runner/%x_%a_log.out
 #SBATCH -e logs_auto_runner/%x_%a_log.err
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=16
 #SBATCH --time=20:00:00
 #SBATCH --mem-per-cpu=4G
-#SBATCH -p physik-fleischhauer
+#SBATCH -p epyc-256
 #SBATCH --mail-type=FAIL,END
-#SBATCH --array=1
+#SBATCH --array=1-100
 
 id=$SLURM_ARRAY_TASK_ID
 
