f5dc3d4a0743b1ed653c0762c0a5ab7b52300594
Ak-ash22 <14akash2000@gmail.com>
2025-03-04 17:35:06 +0100

    Cluster Run Trial 3

--- codework/5atoms_code/error_detection_and_correction.jl
+++ codework/5atoms_code/error_detection_and_correction.jl
@@ -30,6 +30,8 @@ function main(N_trajectories::Int)
     num_timesteps = length(tspan)
     population_data = Dict(key => zeros(num_timesteps) for key in (:a, :b,  :c, :a1, :a2))
     
+    fidelity_data = nothing
+    
     println("Starting the simulation...")
 
     for i in 1:1
@@ -65,7 +67,7 @@ function main(N_trajectories::Int)
 
         #Correcting Atom A
         if ancilla1_population[end] == 1.0
-            fidelity_data = zeros(length(tspan2))
+            # fidelity_data = zeros(length(tspan2))
             f1 = f_correct_factory(1)
             @time tout, ψt = timeevolution.mcwf_dynamic(tspan2,ψ0_ket,f1,maxiters=1e9,seed=(N_trajectories*1000 + i))
 
@@ -73,7 +75,7 @@ function main(N_trajectories::Int)
         
         #Correcting Atom C
         elseif ancilla2_population[end] == 1.0
-            fidelity_data = zeros(length(tspan2))
+            # fidelity_data = zeros(length(tspan2))
             f1 = f_correct_factory(3)
             @time tout, ψt = timeevolution.mcwf_dynamic(tspan3,ψ0_ket,f1,maxiters=1e9,seed=(N_trajectories*1000 + i))
 
@@ -81,7 +83,7 @@ function main(N_trajectories::Int)
         
         #Correcting Atom B
         elseif ancilla1_population[end] == 1.0 && ancilla2_population[end] == 1.0
-            fidelity_data = zeros(length(tspan3))
+            # fidelity_data = zeros(length(tspan3))
             f1 = f_correct_factory(2)
             @time tout, ψt = timeevolution.mcwf_dynamic(tspan2,ψ0_ket,f1,maxiters=1e9,seed=(N_trajectories*1000 + i))
            
