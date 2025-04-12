16e33465ea2379961785c06d8073698b23585b5c
Ak-ash22 <14akash2000@gmail.com>
2025-04-12 10:21:09 +0200

    rounding digits reduced

--- codework/5atoms_code/cluster_data_analysis/full_code_analysis.jl
+++ codework/5atoms_code/cluster_data_analysis/full_code_analysis.jl
@@ -84,9 +84,9 @@ function save_jump_files()
         try
             @load file_path has_error detected_error has_correction_error population_data corrected_population_data fidelity_data end_time # Load the dictionary
 
-            if has_error
+            if has_error && detected_error
                 # Save the jumps data to a new file
-                jump_file_path = jump_folder * "jumps_" * string(i) * ".jld2"
+                jump_file_path = jump_folder * "jumps_corrected_" * string(i) * ".jld2"
                 @save jump_file_path has_error population_data corrected_population_data fidelity_data end_time
             end
 
@@ -96,9 +96,9 @@ function save_jump_files()
                 @save jump_file_path has_correction_error population_data corrected_population_data fidelity_data end_time
             end
 
-            if detected_error
+            if !has error && detected_error
                 # Save the jumps data to a new file
-                jump_file_path = jump_folder * "jumps_detected_error_" * string(i) * ".jld2"
+                jump_file_path = jump_folder * "jumps_detection_error_" * string(i) * ".jld2"
                 @save jump_file_path detected_error population_data corrected_population_data fidelity_data end_time
             end
 
