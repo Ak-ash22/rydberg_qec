502b70b1617ad95e774af217e9ff73cd2726c771
Ak-ash22 <14akash2000@gmail.com>
2025-06-24 13:41:32 +0200

    cluster run 10

--- codework/phaseflip_code/error_detection_and_correction.jl
+++ codework/phaseflip_code/error_detection_and_correction.jl
@@ -239,8 +239,13 @@ function main(N_trajectories::Int)
     ρ_final = ptrace(ψt_end, [4,5])
 
 
-    ψ_target = α .* reduce(kron,[plus,plus,plus,a,a]) - β .* reduce(kron,[minus,minus,minus,a,a])
-    ρ_target = ptrace(Ket(full_basis,ψ_target), [4,5])
+    if ancilla1 == 0.0 && ancilla2 == 0.0
+        ψ_target = α .* reduce(kron,[plus,plus,plus,a,a]) + β .* reduce(kron,[minus,minus,minus,a,a])
+        ρ_target = ptrace(Ket(full_basis,ψ_target), [4,5])  
+    else
+        ψ_target = α .* reduce(kron,[plus,plus,plus,a,a]) - β .* reduce(kron,[minus,minus,minus,a,a])
+        ρ_target = ptrace(Ket(full_basis,ψ_target), [4,5])
+    end
 
     fidelity_after_correction = real(tr(sqrt(sqrt(ρ_final.data)*ρ_target.data*sqrt(ρ_final.data)))^2)
     println("Fidelity after correction is $(fidelity_after_correction)")
