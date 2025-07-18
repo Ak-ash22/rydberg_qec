edfd23c82d20b7f29f3d91da06118082c52f62b9
Ak-ash22 <14akash2000@gmail.com>
2025-07-18 09:59:21 +0200

    relocate first avg files

--- codework/phaseflip_code/cluster_data_analysis/analysis_avg.jl
+++ codework/phaseflip_code/cluster_data_analysis/analysis_avg.jl
@@ -74,7 +74,7 @@ function average_populations(dephase, case, ϕ)
     avg_fidelity_after_correction *= 1 / num_files
 
     # --- Save Averaged Data ---
-    final_file_path = base_path * "N_atoms=5_γ_dephase=$(dephase)_phase=$(ϕ)_Ntraj=$(num_files)_avg.jld2"
+    final_file_path = "/scratch/roq68sum/5atoms_code/phaseflip_code/avg_analysis/dephase_$(dephase)/case$(case)/N_atoms=5_γ_dephase=$(dephase)_phase=$(ϕ)_Ntraj=$(num_files)_avg.jld2"
     @save final_file_path avg_population_data avg_corrected_population_data avg_final_step_population_data avg_S1_data avg_S2_data avg_fidelity_after_encoding avg_fidelity_after_correction # avg_fidelity_data
 
     println("Averaged populations saved to $final_file_path")
