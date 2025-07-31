139bf86f0d5b9088ca41f48df2f1ca0a3916a3e9
Ak-ash22 <14akash2000@gmail.com>
2025-07-31 12:02:17 +0200

    few storage files and shor code

--- codework/shor_code/functions.jl
+++ codework/shor_code/functions.jl
@@ -1,6 +1,6 @@
 include("system_params.jl")
 include("dependencies.jl")
-const Ω, γ_Decay, γ_dephase, V_nn, δ, Δ1_0, T1, T2, T3 = unpack_params()
+const Ω, γ_Decay, γ_dephase, V_nn, Δ_0, T1 = unpack_params()
 
 ##Helper Functions 
 function full_operator(gate, total_qubits, sites)
@@ -37,19 +37,10 @@ end
 
 
 struct qubit_parameters
-    Ω::Float64
     γ_Decay::Float64
     γ_dephase::Float64
     V_nn::Float64
-    δ::Float64
-end
-
-struct ancilla_parameters
-    Ω::Float64
-    γ_Decay::Float64
-    γ_dephase::Float64
-    V_nn::Float64
-    δ::Float64
+    Δ_0::Float64
 end
 
 
@@ -59,35 +50,40 @@ function get_qubit_parameters(p::qubit_parameters,t::Float64,mode::Symbol)
    
     Modes:
         - `:T1` → 0<=t<=T1: Pulse for the qubits A-B-C
-        - `:T2` → T1<=t<=T2: Pi/2 pulse for the qubits A-B-C
-        - `:T3` → T2<=t<=T3: Pulse for the qubits 1-2-3-4-5-6
+        - `:T2` → 0<=t<=T2: Pulse for the ancillas 1-2
+        - `:T3` → 0<=t<=T3: Pulse for the qubits A or C
+        - `:T4` → 0<=t<=T3: Pulse for the qubit B
 
     Parameters: 
         - p:: qubit_parameters → Struct containing the qubit parameters
         - time t:: Float64
-        - mode:: Symbol → Selects phase (`:T1` or `:T2` or `:T3`)
+        - mode:: Symbol → Selects phase (`:T1` or `:T2` or `:T3` or `:T4`)
 
     Returns: 
         - parameters at time t:: Tuple
     """
 
-    @assert mode in [:T1, :T2, :T3] "Invalid mode selected"
+    @assert mode in [:T1, :T2, :T3, :T4a, :T4b, :T5] "Invalid mode selected"
+
+    #### Qubit Encoding Modes
     if mode == :T1
-        Δt = Δ1_0 - p.δ * t
-        return [p.Ω/2, p.Ω/2, Δt, Δt, p.V_nn, p.V_nn]
 
-    elseif mode == :T2
-        return [p.Ω/2, p.Ω/2, -p.V_nn, -p.V_nn, p.V_nn, p.V_nn]
+        if t <= π
+            Ω1 = 1.0
+            Ω2 = 0.0
+        elseif t < (π+sqrt(2)π)
+            Ω1 = 0.0
+            Ω2 = 1.0
+        else 
+            Ω1 = 1.0
+            Ω2 = 0.0
+        end
 
-    elseif mode == :T3
-        Δt = Δ1_0 - p.δ * t
-        return [p.Ω/2, p.Ω/2, p.Ω/2, p.Ω/2, p.Ω/2, p.Ω/2, Δt, Δt, Δt, Δt, Δt, Δt, p.V_nn, p.V_nn, p.V_nn, p.V_nn, p.V_nn, p.V_nn]
-        # return [p.Ω/2, p.Ω/2, Δt, Δt, p.V_nn, p.V_nn]
+        return [Ω1/2, Ω2/2, Ω2/2, p.Δ_0, p.V_nn, Ω2/2, Ω2/2, p.Δ_0, p.V_nn]
     end
-
 end
 
-#Lindbald Operators
+################################################################################################ Lindbaldian Operators
 function lindbaldian_decay(γ_Decay::Float64,site::Array)
     """
     Function to calculate the Lindbaldian decay operator for the MCWF method.
@@ -100,11 +96,11 @@ function lindbaldian_decay(γ_Decay::Float64,site::Array)
     Returns:
         C:: Array{Matrix}: Array of decay operators acting on the system
     """
-    basis = NLevelBasis(2)
+    basis = NLevelBasis(3)
     σ_minus = transition(basis,1,2)
     σ_minus = Operator(σ_minus.basis_l, σ_minus.basis_r, SparseMatrixCSC{ComplexF32, Int64}(σ_minus.data))
 
-    identity = transition(basis,1,1) + transition(basis,2,2)
+    identity = transition(basis,1,1) + transition(basis,2,2) + transition(basis,3,3)
     identity = Operator(identity.basis_l, identity.basis_r, SparseMatrixCSC{ComplexF32, Int64}(identity.data))
     C = Vector{Operator}(undef, total_qubits)
     
@@ -119,36 +115,99 @@ function lindbaldian_decay(γ_Decay::Float64,site::Array)
     return C
 end
 
+function lindbaldian_dephase(γ_dephase::Float64,total_qubits::Int64,site::Array)
+    """
+    Function to calculate the Lindbaldian dephase operator for the MCWF method.
+    The dephase operators are returned according to given respective sites.
 
-basis = NLevelBasis(2)
-n = transition(basis,2,2)
-n = Operator(n.basis_l, n.basis_r, SparseMatrixCSC{ComplexF32, Int64}(n.data))
+    Args:
+        γ_dephase:: Float64: Dephase rate
+        site:: Array: Array of sites at which the decay operator acts
 
-σx = transition(basis,1,2) + transition(basis,2,1)
-σx = Operator(σx.basis_l, σx.basis_r, SparseMatrixCSC{ComplexF32, Int64}(σx.data))
-σy = -im * transition(basis,1,2) + im * transition(basis,2,1)
+    Returns:
+        C:: Array{Matrix}: Array of dephase operators acting on the system
+    """
+    basis = NLevelBasis(3)
+    σ_z= transition(basis,1,1) - transition(basis,2,2) 
+    σ_z = Operator(σ_z.basis_l, σ_z.basis_r, SparseMatrixCSC{ComplexF32, Int64}(σ_z.data))
 
-σx_a = full_operator(σx, total_qubits, [1])
-# σx_b = full_operator(σx, total_qubits, [2])
-σx_c = full_operator(σx, total_qubits, [3])
+    identity = transition(basis,1,1) + transition(basis,2,2) + transition(basis,3,3)
+    identity = Operator(identity.basis_l, identity.basis_r, SparseMatrixCSC{ComplexF32, Int64}(identity.data))
+    C = Vector{Operator}(undef, total_qubits)
+    
+    for i in 1:total_qubits
+        if i in site
+            C[i] = sqrt(γ_dephase) .* full_operator(σ_z, total_qubits, [i])
+        else
+            C[i] = full_operator(identity, total_qubits, [i])
+        end
+    end
 
-σy_a = full_operator(σy, total_qubits, [1])
-σy_b = full_operator(σy, total_qubits, [2])
-σy_c = full_operator(σy, total_qubits, [3])
+    return C
+end
 
-const n_a = full_operator(n, total_qubits, [1])
-const n_b = full_operator(n, total_qubits, [2])
-const n_c = full_operator(n, total_qubits, [3])
+# Matrix Constants to define the Hamiltonians and Lindbaldian operators
+basis = NLevelBasis(3)
+n_r = transition(basis, 3, 3)
+n_r = Operator(n_r.basis_l, n_r.basis_r, SparseMatrixCSC{ComplexF32, Int64}(n_r.data))
 
-nn_ab = full_operator(n, total_qubits, [1,2])
-nn_bc = full_operator(n, total_qubits, [2,3])
+σx_1r = transition(basis, 2, 3) + transition(basis, 3, 2)
+σx_1r = Operator(σx_1r.basis_l, σx_1r.basis_r, SparseMatrixCSC{ComplexF32, Int64}(σx_1r.data))
 
-p = qubit_parameters(Ω,γ_Decay,γ_dephase,V_nn,δ)
+σx_0r = transition(basis, 1, 3) + transition(basis, 3, 1)
+σx_0r = Operator(σx_0r.basis_l, σx_0r.basis_r, SparseMatrixCSC{ComplexF32, Int64}(σx_0r.data))
 
-const tspan = [0.0:0.1:(T1+T2+T3);]
+# Parameters for the Atoms -- coefficients for the Hamiltonian
+p = qubit_parameters(γ_Decay, γ_dephase, V_nn, Δ_0)
+
+
+################################################################################################ Step 1: Encoding A-B-C
+#Timespan for driving atoms A-B-C
+const tspan1 = [0.0:0.1:T1;]
+
+#####System Hamiltonian 1 - driving atoms A-C
+## Driving Atom A
+σx_0r_atom1 = full_operator(σx_0r, total_qubits, [1])
+σx_1r_atom1 = full_operator(σx_1r, total_qubits, [1])
+const n_r_atom1 = full_operator(n_r, total_qubits, [1])
+nn_r12 = full_operator(n_r, total_qubits, [1,2])
+
+#Driving Atom B
+σx_0r_atom2 = full_operator(σx_0r, total_qubits, [2])
+σx_1r_atom2 = full_operator(σx_1r, total_qubits, [2])
+const n_r_atom2 = full_operator(n_r, total_qubits, [2])
+
+##Driving Atom C
+σx_0r_atom3 = full_operator(σx_0r, total_qubits, [3])
+σx_1r_atom3 = full_operator(σx_1r, total_qubits, [3])
+const n_r_atom3 = full_operator(n_r, total_qubits, [3])
+nn_r23 = full_operator(n_r, total_qubits, [2,3])
 
 const coeff1 = [t->get_qubit_parameters(p,t,:T1)]
-const H1 = LazySum([coeff1[1](tspan[1])[i] for i ∈ 1:6],[σx_a, σx_c, n_a, n_c, nn_ab, nn_bc])
+const H1 = LazySum([coeff1[1](tspan1[1])[i] for i ∈ 1:9],[σx_1r_atom2, σx_0r_atom1, σx_1r_atom1, n_r_atom1, nn_r12, σx_0r_atom3, σx_1r_atom3, n_r_atom3, nn_r23])
+
+function Ht1(t)
+    """
+    Function to calculate the time dependent Hamiltonian for the MCWF method till time steps T1+T2.
+        -- H1: Hamiltonian for driving atoms A-B-C for time 0:T1
+        -- H2: Hamiltonian for applying hadamards on atoms A-B-C for time T1:T2
+        -- H3: Hamiltonian for driving atoms 1-2 for time T2:T3
+    
+    Args:
+        t:: Float64: Time
+    
+    Returns:
+        H:: LazySum: Time dependent Hamiltonian
+    """
+
+    coeffs = coeff1[1](t)
+    for i in eachindex(coeffs)
+        H1.factors[i] = coeffs[i]
+    end
+    return H1
+
+end
+
 
 
 σx_1 = full_operator(σx, total_qubits, [4])
--- codework/shor_code/system_params.jl
+++ codework/shor_code/system_params.jl
@@ -1,15 +1,17 @@
 const n_qubits = 9
-const n_ancillas = 6
+const n_ancillas = 8
 const total_qubits = n_qubits + n_ancillas
 
-const g = [1, 0]
-const r = [0, 1]
+const a = ComplexF64[1,0,0]
+const b = ComplexF64[0,1,0]
+const r = ComplexF64[0,0,1]
 
 const α = 0                 # Coefficient of |g> state
 const β = sqrt(1-α^2)       # Coefficient of |r> state
+const ϕ = 0
 
 
-function wavefunction(num_qubits::Int64, α, β, site::Int64)
+function wavefunction(num_qubits::Int64, α, β, ϕ::Float64, site::Int64)
     """
     Function to initialize the wavefunction of the N-qubit system with given parameters
     initialized at given site.
@@ -26,11 +28,11 @@ function wavefunction(num_qubits::Int64, α, β, site::Int64)
 
     @assert 1<=site<=num_qubits "Site should be between 1 and $num_qubits"
     @assert 1<=num_qubits "Number of qubits should be greater than 0"
-    @assert 0<=α<=1 "α should be between 0 and 1"
-    @assert 0<=β<=1 "β should be between 0 and 1"
+    @assert 0.0<=α<=1.0 "α should be between 0 and 1"
+    @assert 0.0<=β<=1.0 "β should be between 0 and 1"
 
-    site_states = [g for _ in 1:num_qubits]
-    site_states[site] = α.*g + β.*r
+    site_states = [a for _ in 1:num_qubits]
+    site_states[site] = α .* b + (exp(1im * ϕ) * β) .* a
 
     ψ = reduce(kron, site_states)
     return ψ
@@ -42,16 +44,16 @@ function initialize_system()
     System:
 
     1    3    5
-     a1  b3  c5   -----ancillas
-      A--B--C    
-     a2  b4  c6   -----ancillas
+     a1   b3  c5   -----ancillas
+      A-p1-B-p2-C    
+     a2   b4  c6   -----ancillas
     2    4    6    
 
     ψ_system = tensor(A,B,C,1,2,3,4,5,6,a1,a2,b3,b4,c5,c6)
 
     Returns: The wavefunction of the system at t=0.
     "
-    ψ_system = wavefunction(total_qubits,α,β,2)
+    ψ_system = wavefunction(total_qubits,α,β,ϕ,site=2)
 
     return ψ_system
 end
@@ -62,17 +64,13 @@ function params()
     "
     return Dict(
         :Ω => 1.0,             # Rabi frequency            
-        :γ_Decay => 1e-3,        # Decay rate on the qubits
+        :γ_Decay => 0.0,        # Decay rate on the qubits
         :γ_dephase => 0.0,      # Dephasing rate on the qubits
-        :V1_nn => -1000.0,      # rydberg interaction on the qubits
-        # :V2_nn => -1000.0,      # rydberg interaction on the ancillas
-        :δ1 => 0.417,            # adiabtatic sweep rate for qubits
-        # :δ2 => 0.108,            # adiabtatic sweep rate for ancillas
-        :Δ1_0 => 1032.0,        # Detuning at t=0 for qubits
-        # :Δ2_0 => 1032.0,        # Detuning at t=0 for ancillas
-        :T1 => 154.0,    # Evolution time for step 1 -- From 2atom_optimal_decay_result.ipynb
-        :T2 => pi/2,     # Evolution time for step 2
-        :T3 => 154.0     # Evolution time for step 3
+        :V_nn => -1000.0,      # rydberg interaction on the qubits
+        :Δ0_0 => 1000.0,        # Detuning at t=0 for encoding qubits and ancillas (CNOT gate)
+        :T1 => (π + sqrt(2)π + π),    # Evolution time for step 1 -- Encoding A-B-C
+        # :T2 => pi/2,     # Evolution time for step 2
+        # :T3 => 154.0     # Evolution time for step 3
         # :T4 => 582.0     # Evolution time for step 4
         # :T5 => 582.0     # Evolution time for step 5  -- for phase flip error syndrome
         # :T6 => 582.0     # Evolution time for step 6  -- for phase flip error syndrome
@@ -84,5 +82,5 @@ function unpack_params()  #Need to define these as const in main.jl
     Unpack the system parameters
     "
     p = params()
-    return p[:Ω], p[:γ_Decay], p[:γ_dephase], p[:V1_nn], p[:δ1], p[:Δ1_0], p[:T1], p[:T2], p[:T3]
+    return p[:Ω], p[:γ_Decay], p[:γ_dephase], p[:V_nn], p[:Δ_0], p[:T1]
 end
